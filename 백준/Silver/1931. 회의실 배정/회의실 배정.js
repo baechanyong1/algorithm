c03bcdb5224f7811c314378786d86e21fe129e87
chan-yong <110149885+baechanyong1@users.noreply.github.com>
2024-07-01 20:24:47 +0900

    [Silver I] Title: 회의실 배정, Time: 372 ms, Memory: 43696 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -0,0 +1,22 @@
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().split('\n')
+
+let n = input[0]
+let arr = []
+
+for(i=1; i<=n; i++){
+    arr.push(input[i].split(' ').map(Number))
+}
+
+arr.sort(function(a,b){
+    if(a[1]!=b[1]) return a[1]-b[1]
+    else return a[0] - b[0]
+})
+let cnt = 1, cur = 0
+for(i=1; i<n;i++){
+    if(arr[cur][1]<=arr[i][0]){
+        cnt ++
+        cur = i
+    }
+}
+console.log(cnt)
