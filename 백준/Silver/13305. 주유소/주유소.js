5d85bd1b8a56dda76668c1b1236140ee9bd52d9a
chan-yong <110149885+baechanyong1@users.noreply.github.com>
2024-07-01 21:37:00 +0900

    [Silver III] Title: 주유소, Time: 264 ms, Memory: 36688 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,18 +1,18 @@
 let fs = require('fs')
 let input = fs.readFileSync('/dev/stdin').toString().split('\n')
 
-let n = input[0]
+let n = Number(input[0])
 let dist = input[1].split(' ').map(Number)
 let cost = input[2].split(' ').map(Number)
 
 let minCost = cost[0]
-for(let i = 1; i < n ; i++){
+for(let i = 0; i < n ; i++){
     minCost = Math.min(minCost, cost[i])
     cost[i] = minCost
 }
 
-let sum = 0
+let sum = BigInt(0)
 for(let i = 0; i < n-1 ; i++){
-    sum += cost[i] * dist[i]
+    sum += BigInt(cost[i]) * BigInt(dist[i])
 }
-console.log(sum)
+console.log(String(sum))
