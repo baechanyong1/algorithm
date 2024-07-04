d64d74887aa3e3304c91b68060a7db1416f8b7ac
chan-yong <110149885+baechanyong1@users.noreply.github.com>
2024-07-04 11:53:54 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,12 @@
+function solution(num) {
+    var answer = 0;
+while(num!==1 && answer < 500){
+    if (num%2==0){
+        num/=2
+    }else {
+        num = num*3+1
+    }
+    answer++
+}
+    return num === 1 ? answer : -1
+}
