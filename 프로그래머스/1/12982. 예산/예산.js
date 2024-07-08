fea5bc62f6a9ff84a1456b55b0c21824a5d6e133
chan-yong <110149885+baechanyong1@users.noreply.github.com>
2024-07-08 16:37:01 +0900

    [level 1] Title: 예산, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,11 @@
+function solution(d, budget) {
+    var answer = 0;
+    d.sort((a,b)=>a-b)
+    for(let i = 0 ; i < d.length ; i++){
+        if(budget>=d[i]){
+            answer++
+            budget -= d[i]
+        }
+    }
+    return answer;
+}
