831125eafe30566cf49e1e25cc0b7143de93a7a8
chan-yong <110149885+baechanyong1@users.noreply.github.com>
2024-07-13 22:08:46 +0900

    [level 2] Title: 짝지어 제거하기, Time: 41.29 ms, Memory: 43.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,13 @@
+function solution(s)
+{
+    var answer = 0;
+    let arr = []
+    for(let i = 0 ; i < s.length ; i++){
+        if(arr.length > 0 && arr[arr.length - 1] === s[i]){
+            arr.pop()
+        } else {
+            arr.push(s[i])
+        }
+        }
+    return arr.length == 0 ? 1 : 0
+}
