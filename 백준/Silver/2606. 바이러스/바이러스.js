8e82fbe459ed8a9ccf0a173323ab16d6caf9592b
chan-yong <110149885+baechanyong1@users.noreply.github.com>
2024-08-07 14:49:55 +0900

    [Silver III] Title: 바이러스, Time: 88 ms, Memory: 9432 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -0,0 +1,24 @@
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().split('\n')
+
+let n = Number(input[0])
+let m = Number(input[1])
+let graph = []
+for(let i = 1 ; i <= n ; i++) graph[i] = []
+for(let i = 2 ; i <= m+1 ; i++){
+    let [x,y] = input[i].split(' ').map(Number)
+    graph[x].push(y)
+    graph[y].push(x)
+}
+
+let cnt = 0
+let visit = new Array(n+1).fill(false)
+function dfs(x){
+    visit[x]=true
+    cnt++
+    for(y of graph[x]){
+        if(!visit[y]) dfs(y)
+    }
+}
+dfs(1)
+console.log(cnt-1)
